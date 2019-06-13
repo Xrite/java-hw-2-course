2908d86c9fe3310a8e68f566685e9ea5131b9782
Anton Bukov <bukov.anton@gmail.com>
2019-06-13 22:55:36 +0300

    add javadoc

--- 9-MyJUnit/src/main/java/annotations/Test.java
+++ 9-MyJUnit/src/main/java/annotations/Test.java
@@ -7,19 +7,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Annotation for methods that should be tested
- * ignored should be true if method should not be tested
- * reason contains a reason for method not be tested
- * exception is an expecting exception
- * test will be failed if another exception will be thrown
- */
+/** Annotation for methods that should be tested */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
+    /** Expecting exception. Test will be failed if another exception will be thrown */
     Class<? extends Throwable> exception() default NoException.class;
 
+    /** Should be true if method should not be tested. False by default */
     boolean ignored() default false;
 
+    /** Reason for method not be tested. Makes sense only if test is ignored */
     String reason() default "N/A";
 }
